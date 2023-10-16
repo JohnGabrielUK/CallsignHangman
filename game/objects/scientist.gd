fae6a564e4326567f0df85da0a62b2626e63106d
John Gabriel <accounts@johngabriel.co.uk>
2023-10-16 18:19:23 +0100

    Add scientist model and animations

--- game/objects/scientist.gd
+++ game/objects/scientist.gd
@@ -10,6 +10,8 @@ const FOLLOW_THRESHOLD : float = 1.5
 
 enum State {NOT_YET_MET, FOLLOWING_PLAYER, WAITING}
 
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
+
 @export var id : int
 
 @onready var current_state : int = State.NOT_YET_MET
@@ -28,6 +30,10 @@ var following : Node3D
 func rescue() -> void:
 	emit_signal("rescued", id)
 	queue_free()
+
+func switch_animation_if_not_current(anim_name : String, blend_amount : float) -> void:
+	if anim_player.current_animation != anim_name:
+		anim_player.play(anim_name, blend_amount)
 
 func interact(by_whom : Node3D) -> void:
 	match current_state:
@@ -63,12 +69,17 @@ func _physics_process_following_player(delta : float) -> void:
 		horizontal_position.y = global_position.y
 		look_at(horizontal_position, Vector3(0, 1, 0), true)
 		move_and_collide(-Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * MOVE_SPEED * delta)
+		switch_animation_if_not_current("jog", 0.25)
 	else:
 		var horizontal_position = following.global_position
 		horizontal_position.y = global_position.y
 		look_at(horizontal_position, Vector3(0, 1, 0), true)
+		switch_animation_if_not_current("idle", 0.25)
 		
 
 func _physics_process(delta : float) -> void:
 	match current_state:
 		State.FOLLOWING_PLAYER: _physics_process_following_player(delta)
+
+func _ready() -> void:
+	anim_player.play("idle")
