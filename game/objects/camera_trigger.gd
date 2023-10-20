e4e3a09405fec0635154031fefbb3b9826d065de
John Gabriel <accounts@johngabriel.co.uk>
2023-10-20 18:53:09 +0100

    Fix camera bug and add lights to more rooms

--- game/objects/camera_trigger.gd
+++ game/objects/camera_trigger.gd
@@ -5,3 +5,8 @@ signal player_entered
 func _on_body_entered(body : Node3D) -> void:
 	if body is Player:
 		emit_signal("player_entered")
+
+func check_for_player() -> void:
+	for body in get_overlapping_bodies():
+		if body is Player:
+			emit_signal("player_entered")
