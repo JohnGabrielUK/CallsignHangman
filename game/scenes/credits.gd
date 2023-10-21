7f5a2cf4fc23641e8b5a602cb08bb8bd9670d8d9
John Gabriel <accounts@johngabriel.co.uk>
2023-10-21 23:32:26 +0100

    Add credits and game over music

--- game/scenes/credits.gd
+++ game/scenes/credits.gd
@@ -3,4 +3,8 @@ extends Control
 @onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
 	anim_player.play("credits")
+
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
