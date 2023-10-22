aea3ff7e6e9989415500a62596a388977623ade4
John Gabriel <accounts@johngabriel.co.uk>
2023-10-22 19:22:41 +0100

    Change credits, add sfx to room exit

--- game/objects/room_exit.gd
+++ game/objects/room_exit.gd
@@ -3,5 +3,8 @@ extends StaticBody3D
 @export var destination_room : String
 @export var destination_spawn_id : int
 
+@onready var audio_open : AudioStreamPlayer3D = $Audio_Open
+
 func interact(by_whom : Node3D) -> void:
 	get_tree().call_group("game_controller", "change_room", destination_room, destination_spawn_id)
+	audio_open.play()
